d20e52920498ab1814e61386d08f71e98de6323e
hufflk96 <118147274+hufflk96@users.noreply.github.com>
2023-06-01 18:41:31 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+### Description
+
+React Professional Portfolio LKH
+
+### Features
+
+- Fully Responsive
+- Multi-Pages
+- Contact Me/About Me
+- React App
+- Portfolio
+
+### Instalation
+
+Clone the Code
+
+<pre>git clone https://github.com/hufflk96/reactportfolio.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+Start the server
+
+<pre>yarn start</pre>
+
+ 
---
+++ src/content_option.js
@@ -0,0 +1,101 @@
+const logotext = "Kate";
+const meta = {
+    title: "Lorena Huff",
+    description: "I’m Lorena Huff! I am a UNC Chapel Hill Bootcamp Software Developer Student!",
+};
+
+const introdata = {
+    title: "I’m Lorena Huff",
+    animated: {
+        first: "I love technology",
+        second: "I have a passion for games",
+        third: "I give my all",
+    },
+    description: "Computers are incredibly fast, accurate and stupid; humans are incredibly slow, inaccurate and brilliant. The marriage of the two is a force beyond calculation.",
+    your_img_url: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg",
+};
+
+const dataabout = {
+    title: "All About Who I Am",
+    aboutme: "My name is Lorena Huff! I graduated with a Bachelors of Science in Agricultural Business with a concentration in Pre-Law and a Minor in Animal Science from the University of Arkansas. I moved to the RTP area in the beginning of 2022 and absoultely love living here. I have always had a passion and love for technology and video games which lead me to wanting to learn more about that world. I began taking IT classes and high school and went on to earn two different cloud certifications after my college graduation to better understand all aspects of the computer world. I now curerntly work as a sales associate and strive to learn new things!",
+};
+
+const skills = [{
+        name: "AWS",
+        value: 90,
+    },
+    {
+        name: "Microsoft Azure",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "MYSQL",
+        value: 75,
+    },
+];
+
+const dataportfolio = [{
+        img: "https://user-images.githubusercontent.com/118147274/239769375-749818a5-b12e-480b-b9a1-78810b1e3323.png",
+        description: "Weather Forecast",
+        link: "https://hufflk96.github.io/Weatherfordays/",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/118147274/233714602-3d395087-43ef-4f46-8d60-5bec9782cbb1.png",
+        description: "Javascript Quiz",
+        link: "https://hufflk96.github.io/javascript-quiz/",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/118147274/224871310-07a17a1d-ff25-4a5a-9059-25af67f61e4d.png",
+        description: "HTML REfactor",
+        link: "https://hufflk96.github.io/horiseon-one-hw1/",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/118003612/213340134-bde02a31-e398-44bb-9135-a0c9fd2d40ab.png",
+        description: "Entertained",
+        link: "https://acarter867.github.io/Entertained/",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/118147274/232848295-a5f13c10-389e-45cf-8b6b-84ce9a2b90e6.png",
+        description: "Password Creator",
+        link: "https://hufflk96.github.io/password-creator/",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/118147274/233679740-9d1f106c-1165-473e-b42c-bcaed5db7633.png",
+        description: "Day Planner",
+        link: "https://hufflk96.github.io/workday-planner-main/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hufflk96@gmail.com",
+    YOUR_FONE: "(870-394-6850)",
+    description: "Feel free to email me or follow me on socials! "
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
